8b8181b62b5ea93dfb031579441592416914f52a
kazet <kazet15@gmail.com>
2015-08-30 14:59:55 +0200

    add map and api

---
+++ public/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  var map = L.map('map').setView([54.371675,18.616328], 15);
+  var events = [];
+  var markers = [];
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  var drawMarker = function (event) {
+    return new L.marker([event.location.lat, event.location.lon]).addTo(map);
+  };
+
+  var redraw = function () {
+    markers.forEach(function (marker) {
+      map.removeLayer(marker);
+    });
+
+    markers = [];
+
+    events.forEach(function (event) {
+      markers.push(drawMarker(event));
+    });
+  };
+
+
+  $.get('/events', function (data) {
+    events = data;
+    redraw();
+  });
+});
--- server.js
+++ server.js
@@ -4,6 +4,29 @@ var config = require('config');
 
 var app = express();
 
+app.get('/events', function (req, res) {
+  var events = [];
+
+  res.set({
+    'content-type': 'application/json'
+  });
+
+  events.push({
+    location: {
+      lat: 54.371675,
+      lon: 18.616328
+    }
+  });
+
+  res.send(JSON.stringify(events));
+});
+
+app.post('/events', function (req, res) {
+
+});
+
+app.use(express.static('public'));
+
 app.listen(config.get('port'), function () {
     console.log('server up');
 });
